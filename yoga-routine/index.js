be0e9c604863504cea3aa5266bb3f24636a88276
Nadir <contact@mestarico.com>
2024-03-29 17:30:00 +0100

    Finish

--- yoga-routine/index.js
+++ yoga-routine/index.js
@@ -1,8 +1,5 @@
-
-
-const main = document.querySelector("main")
-
-let exercices = [
+const main = document.querySelector("main");
+const basicArray = [
     { pic: 0, min: 1 },
     { pic: 1, min: 1 },
     { pic: 2, min: 1 },
@@ -14,98 +11,182 @@ let exercices = [
     { pic: 8, min: 1 },
     { pic: 9, min: 1 },
 ];
+let exerciceArray = [];
 
-class Physiqueexo { }
+// Get stored exercices array
+(() => {
+    if (localStorage.exercices) {
+        exerciceArray = JSON.parse(localStorage.exercices);
+    } else {
+        exerciceArray = basicArray;
+    }
+})();
 
-const utile = {
-    page: function (title, content, btn) {
-        document.querySelector("h1").innerHTML = title
-        main.innerHTML = content
-        document.querySelector(".btn-container").innerHTML = btn
-    },
-
-    eventnumber: function () {
-        document.querySelectorAll('input[type="number"]').forEach((input) => {
-            input.addEventListener("input", (e) => {
-                exercices.map((exo) => {
-                    if (exo.pic == e.target.id) {
-                        exo.min = parseInt(e.target.value)
-                        console.log(exercices)
-                    }
-                })
-            })
-        })
-
-    },
-
-    arromove: function () {
-        document.querySelectorAll(".arrow").forEach((arr) => {
-            arr.addEventListener("click", (e) => {
-                let pos = 0;
-                exercices.map((exo) => {
-                    if (exo.pic == e.target.dataset.pic) {
-
-                        [exercices[0], exercices[1]] = [exercices[1], exercices[0]]
-                        console.log(exercices)
-                    } else {
-                        pos++;
-
-                    }
-                })
-            })
-        })
+class Exercice {
+    constructor() {
+        this.index = 0;
+        this.minite = exerciceArray[this.index].min;
+        this.seconde = 0;
     }
 
+    compoteARebourde() {
+        this.seconde = this.seconde < "10" ? "0" + this.seconde : this.seconde
+
+
+        setTimeout(() => {
+            if (this.minite === 0 && this.seconde == + "00") {
+                this.index++;
+                this.ring();
+                if (this.index < exerciceArray.length) {
+                    this.minite = exerciceArray[this.index].min
+                    this.seconde = 0
+                    this.compoteARebourde();
+                } else {
+                    return page.finish();
+                }
+            } else if (this.seconde === "00") {
+                this.minite--;
+                this.seconde = 59;
+                this.compoteARebourde();
+            } else {
+                this.seconde--;
+                this.compoteARebourde();
+            }
+
+        }, 10)
+
+        return main.innerHTML =
+            `<div class="exercice-container">
+            <p>${this.minite}:${this.seconde}</p>
+            <img src="./img/${exerciceArray[this.index].pic}.png"/>
+            <div>${this.index + 1}/${exerciceArray.length}</div>
+            </div>
+        
+        
+        `
+    }
+
+    ring() {
+        let audio = new Audio();
+        audio.src = "./ring.mp3"
+        audio.play();
+    }
 
 }
 
-const parama = {
+const utils = {
+    pageContent: function (title, content, btn) {
+        document.querySelector("h1").innerHTML = title;
+        main.innerHTML = content;
+        document.querySelector(".btn-container").innerHTML = btn;
+    },
+
+    handleEventMinutes: function () {
+        document.querySelectorAll('input[type="number"]').forEach((input) => {
+            input.addEventListener("input", (e) => {
+                exerciceArray.map((exo) => {
+                    if (exo.pic == e.target.id) {
+                        exo.min = parseInt(e.target.value);
+                        this.store();
+                    }
+                });
+            });
+        });
+    },
+
+    handleEventArrow: function () {
+        document.querySelectorAll(".arrow").forEach((arrow) => {
+            arrow.addEventListener("click", (e) => {
+                let position = 0;
+                exerciceArray.map((exo) => {
+                    if (exo.pic == e.target.dataset.pic && position !== 0) {
+                        [exerciceArray[position], exerciceArray[position - 1]] = [
+                            exerciceArray[position - 1],
+                            exerciceArray[position],
+                        ];
+                        page.lobby();
+                        this.store();
+                    } else {
+                        position++;
+                    }
+                });
+            });
+        });
+    },
+
+    deleteItem: function () {
+        document.querySelectorAll(".deleteBtn").forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                let newArr = [];
+                exerciceArray.map((exo) => {
+                    if (exo.pic != e.target.dataset.pic) {
+                        newArr.push(exo);
+                    }
+                });
+                exerciceArray = newArr;
+                page.lobby();
+                this.store();
+            });
+        });
+    },
+
+    reboot: function () {
+        exerciceArray = basicArray;
+        page.lobby();
+        this.store();
+    },
+
+    store: function () {
+        localStorage.exercices = JSON.stringify(exerciceArray);
+    },
+};
+
+const page = {
     lobby: function () {
-        let arrayexo = exercices.map((exo) => {
-            return `
+        let mapArray = exerciceArray
+            .map(
+                (exo) =>
+                    `
+        <li>
+          <div class="card-header">
+            <input type="number" id=${exo.pic} min="1" max="10" value=${exo.min}>
+            <span>min</span>
+          </div>
+          <img src="./img/${exo.pic}.png" />
+          <i class="fas fa-arrow-alt-circle-left arrow" data-pic=${exo.pic}></i>
+          <i class="fas fa-times-circle deleteBtn" data-pic=${exo.pic}></i>
+        </li>
+      `
+            )
+            .join("");
 
-<li>
-<div class="card-header">
-<input type="number"  id=${exo.pic} min="1" max="10" value=${exo.min}>
-<span>min</span>
-</div>
-<img src="./img/${exo.pic}.png" alt"image de l'exo ${exo.pic}"/>
-<i class="fas fa-arrow-alt-circle-left arrow" data-pic=${exo.pic} ></i>
-<i class="fas fa-times-circle deleteBtn" data-pic=${exo.pic}></i>
-</li>
-
-`
-        })
-
-        utile.page(
-            "<h1>Paramétrage <i id='reboot' class='fas fa - undo'></i> </h1>",
-            `<ul>${arrayexo}</ul>`,
-            `<button id="start">Commencer <i class="far fa-play-circle"></i> </button>`);
-
-        utile.eventnumber();
-        utile.arromove();
-
-
-
+        utils.pageContent(
+            "Paramétrage <i id='reboot' class='fas fa-undo'></i>",
+            "<ul>" + mapArray + "</ul>",
+            "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
+        );
+        utils.handleEventMinutes();
+        utils.handleEventArrow();
+        utils.deleteItem();
+        reboot.addEventListener("click", () => utils.reboot());
+        start.addEventListener("click", () => this.routine());
     },
 
     routine: function () {
-        utile.page("Routine", "Execices routines", null
+        const exerciiice = new Exercice();
 
-        )
+        utils.pageContent("Routine", exerciiice.compoteARebourde(), null);
     },
 
     finish: function () {
-        utile.page(
-            "C'est terminé",
+        utils.pageContent(
+            "C'est terminé !",
             "<button id='start'>Recommencer</button>",
-            "<button id='reboot' class='btn-reboot'>Rénitialiser <i class='fas fa-times-circle'></i> </button>"
-
-        )
-
+            "<button id='reboot' class='btn-reboot'>Réinintialiser <i class='fas fa-times-circle'></i></button>"
+        );
+        start.addEventListener("click", () => this.routine());
+        reboot.addEventListener("click", () => utils.reboot());
     },
-}
+};
 
-
-
-parama.lobby()
+page.lobby();
